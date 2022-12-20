a1e7a21c30b7cff6be1dbe96af675cfe401acdaf
snerva <selenenerva@live.it>
2022-12-20 17:14:49 +0100

    table data

--- database/migrations/2022_12_20_154307_create_trains_table.php
+++ database/migrations/2022_12_20_154307_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('train_companies', 20);
+            $table->string('train_station_from', 30);
+            $table->string('train_station_to', 30);
+            $table->dateTime('departure_hour',);
+            $table->dateTime('arrival_hour');
+            $table->smallInteger('train_code');
+            $table->tinyInteger('railway_coaches');
+            $table->boolean('on_time')->default(1);
+            $table->string('cancelled', 10);
             $table->timestamps();
         });
     }
