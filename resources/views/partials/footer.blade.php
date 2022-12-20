4632d3702852b0d87fc4698da05e41342ae0bbd9
snerva <selenenerva@live.it>
2022-12-20 16:13:05 +0100

    create table and basic structure

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,31 @@
+<footer class="bg-secondary text-white">
+    <div class="container">
+        <div class="row p-3">
+            <div class="col">
+                <div class="logo">
+                    <h2>TrainLine</h2>
+                </div>
+            </div>
+
+            <div class="col">
+                <h3>Links</h3>
+                <ul class="list-unstyled">
+                    <li>About Us</li>
+                    <li>Train and Bus companies</li>
+                    <li>Stations</li>
+                    <li>Top Destination</li>
+                </ul>
+            </div>
+
+            <div class="col">
+                <h3>Legal</h3>
+                <ul class="list-unstyled">
+                    <li>Privacy Policy</li>
+                    <li>Faqs</li>
+                    <li>Terms and conditions</li>
+                    <li>Contacts</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/trains', function () {
+    return view('trains');
+})->name('trains');
